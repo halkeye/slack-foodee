9d515b37bbc38f26ef0d27b999d69d4f8fe6965d
Gavin Mogan <gavin@gavinmogan.com>
2016-09-17 19:48:06 -0700

    all working all the time

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const app = require('./lib/app')();
+const morgan = require('morgan');
+
+const app = require('./lib/app')([morgan('combined')]);
 app.data = require('./lib/models');
+
+app.SLACK_TOKEN = process.env.SLACK_VERIFICATION_TOKEN;
 
 app.data.syncAll()
   .then(() => {
@@ -7,6 +11,6 @@ app.data.syncAll()
       console.log('Example app listening on port 3000!'); //eslint-disable-line
     });
   })
-  .catch((err) => {
+  .catch(err => {
     console.error('Error syncing/starting app server:', err); //eslint-disable-line
   });
